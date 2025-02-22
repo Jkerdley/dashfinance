7b2a2a42627e24d2d554a6c8eb5f31413e90130c
Jkerdley <jk-erley@yandex.ru>
2025-02-23 01:57:25 +0700

    Добавил текст на кнопки

--- src/components/OperationsPanelButtons/OperationsPanel.jsx
+++ src/components/OperationsPanelButtons/OperationsPanel.jsx
@@ -6,10 +6,10 @@ export const OperationsPanel = ({ onClick }) => {
 		<div id="operations__buttons" className="flex flex-col mt-4 items-start gap-4">
 			<div className="flex gap-4 w-full justify-between items-center">
 				<WideOperationsButton onClick={onClick} color={'bg-[#b9ff80]'} alt="income">
-					<span className="text-lg font-semibold">Купить +</span>
+					<span className="text-lg font-semibold">Покупка +</span>
 				</WideOperationsButton>
 				<WideOperationsButton onClick={onClick} color={'bg-[#ff81b6]'} alt="income">
-					<span className="text-lg font-semibold">Продать - </span>
+					<span className="text-lg font-semibold">Продажа - </span>
 				</WideOperationsButton>
 			</div>
 		</div>
