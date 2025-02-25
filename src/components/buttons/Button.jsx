bd2299ed928b2503b915fd98da8a9194b15db088
Jkerdley <jk-erley@yandex.ru>
2025-02-25 20:49:56 +0700

    правки

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -11,7 +11,7 @@ export const Button = ({ icon, alt, disabled, onClick, children, padding, width 
 	) : (
 		<button
 			onClick={onClick}
-			className={`flex ${width} h-9 ${padding} justify-center items-center  hover:scale-x-110 bg-gradient-to-br from-sky-500 to-blue-900 transition-all ease-in-out duration-100 rounded-xl cursor-pointer`}
+			className={`flex ${width} h-9 ${padding} justify-center items-center hover:scale-x-110 bg-gradient-to-br from-sky-500 to-blue-900 transition-all ease-in-out duration-100 rounded-xl cursor-pointer`}
 		>
 			{children}
 			<img className="h-6" src={icon} alt={alt} />
