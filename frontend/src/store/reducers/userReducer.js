c5ba094baad1419467b36e2ab492489f50ed8124
Jkerdley <jk-erley@yandex.ru>
2025-03-07 23:56:16 +0700

    добавил user в store

--- frontend/src/store/reducers/userReducer.js
+++ frontend/src/store/reducers/userReducer.js
@@ -1,31 +1,15 @@
 import { ACTIONS } from '../actionTypes';
 
 const initialUserState = {
-	isLoading: false,
 	user: [],
-	error: null,
 };
 
 export const userReducer = (state = initialUserState, action) => {
 	switch (action.type) {
-		case ACTIONS.FETCH_ACCOUNTS_REQUEST:
+		case ACTIONS.GET_USER_DATA:
 			return {
 				...state,
-				isLoading: true,
-				error: null,
-			};
-		case ACTIONS.FETCH_ACCOUNTS_SUCCESS:
-			return {
-				...state,
-				isLoading: false,
-				data: action.payload.accounts,
-				error: null,
-			};
-		case ACTIONS.FETCH_ACCOUNTS_ERROR:
-			return {
-				...state,
-				isLoading: false,
-				error: action.payload.error,
+				user: action.payload,
 			};
 
 		default:
