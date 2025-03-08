1e6b6c5e51cb5826b63a7c202f2d4e2faaee9468
Jkerdley <jk-erley@yandex.ru>
2025-03-09 02:16:12 +0700

    Исправил баги

--- frontend/src/components/modalWindow/operationSelectors/OperationSelectors.jsx
+++ frontend/src/components/modalWindow/operationSelectors/OperationSelectors.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const OperationSelectors = ({
 	formState,
-	accountsInCurrency,
-	categoriesInCurrency,
+	accounts,
+	categories,
 	onAccountChange,
 	onCategoryChange,
 }) => {
@@ -14,7 +14,7 @@ export const OperationSelectors = ({
 				value={formState.selectedAccount?.id || ''}
 				onChange={(e) => onAccountChange(e.target.value)}
 			>
-				{accountsInCurrency.map((account) => (
+				{accounts.map((account) => (
 					<option
 						className="bg-[#334864] text-[#daeaff] rounded-lg w-[40%] h-[30px] p-2 transition-all duration-300 ease-in-out hover:bg-[#4b5563] hover:text-white"
 						key={account.id}
@@ -30,7 +30,7 @@ export const OperationSelectors = ({
 				value={formState.selectedCategory?.id || ''}
 				onChange={(e) => onCategoryChange(e.target.value)}
 			>
-				{categoriesInCurrency.map((category) => (
+				{categories.map((category) => (
 					<option
 						className="bg-[#334864] text-[#daeaff] rounded-lg w-[40%] h-[30px] p-2 transition-all duration-300 ease-in-out hover:bg-[#4b5563] hover:text-white"
 						key={category.id}
