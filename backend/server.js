cde789022ecdbcae408e0e65cf7dbfe76d682e49
Jkerdley <jk-erley@yandex.ru>
2025-03-10 04:17:33 +0700

    Подготовка к докеру

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ require("dotenv").config();
 const PORT = 3007;
 const app = express();
 
-// app.use(express.static("../frontend/build"));
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(express.static("../frontend/build"));
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173",
+//         credentials: true,
+//     })
+// );
 
 app.use(cookieParser());
 app.use(express.json());
--- frontend/src/api/CURRENCY_API_URL.js
+++
@@ -1,3 +0,0 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const targetUrl = 'https://currate.ru/api/?get=rates&pairs=USDRUB&key=43b2a5faa0f1d28ab76a409c595f86e4';
-export const CURRENCY_COURSE_API = proxyUrl + targetUrl;
