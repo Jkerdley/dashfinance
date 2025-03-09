4534e7f3e0b703c4bcdb726886b8ef45a45e34b6
Jkerdley <jk-erley@yandex.ru>
2025-03-09 23:17:51 +0700

    Добавил ряд крипто цен

--- frontend/src/pages/CryptoPage/CryptoCards/TopRowCard.jsx
+++ frontend/src/pages/CryptoPage/CryptoCards/TopRowCard.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { calculateValueInCurrency } from '../../../utils';
+import { useCurrency } from '../../../hooks';
+import { CardIcon } from '../../../components/CardIcon';
 
-export const TopRowCard = ({ coinTitle, flex, isAddButton, disabled, onClick }) => {
+export const TopRowCard = ({ assetData, flex, isAddButton, disabled, onClick }) => {
+	const { isUSD, rubleCourse } = useCurrency();
+	const priceInCurrency = calculateValueInCurrency(assetData.price, isUSD, rubleCourse);
+
+	console.log('assetData', assetData);
 	const isDisabled = disabled ? '' : 'cursor-pointer';
 	return isAddButton ? (
 		<button onClick={onClick} className={`group flex flex-1/12 w-auto rounded-3xl ${isDisabled}`}>
@@ -15,6 +22,24 @@ export const TopRowCard = ({ coinTitle, flex, isAddButton, disabled, onClick }) 
 			</div>
 		</button>
 	) : (
-		<div className={`flex py-2 px-4 ${flex} bg-sky-950/40 min-h-26 rounded-3xl`}>{coinTitle}</div>
+		<section className={`flex flex-col py-4 px-4 ${flex} bg-sky-950/40 min-h-26 rounded-3xl`}>
+			<div className="flex flex-col gap-2 items-start">
+				<div className="flex gap-3 items-center">
+					<CardIcon icon={assetData.icon} />
+					<span className="text-2xl font-medium truncate">{assetData.symbol}</span>
+					<span className="text-2xl font-medium truncate"> {priceInCurrency}</span>
+				</div>
+				<div className="flex gap-3 items-center justify-center bg-amber-900">
+					<div className="flex gap-2 items-center bg-amber-500">
+						<span>Рост за неделю:</span>
+						<span
+							className={`text-xl font-medium truncate ${parseFloat(assetData.priceChange1w) < 0 ? 'text-main-red' : 'text-main-green'}`}
+						>
+							{assetData.priceChange1w}
+						</span>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
--- frontend/src/pages/CryptoPage/CryptoLayout.jsx
+++ frontend/src/pages/CryptoPage/CryptoLayout.jsx
@@ -3,18 +3,21 @@ import { CryptoOpreationsHistoryContainer } from './CryptoOperationHistory';
 import { CryptoResultLayout } from './CryptoResult';
 import { MyCriptoPortfolioList } from './CryptoPortfolioList';
 import { TopRowCardsLayout } from '../CryptoPage/CryptoCards/TopRowCardsLayout';
-import { CryptoBalanceChart, TopGainerAndLooserChart } from './Charts';
+import { TopGainerAndLooserChart } from './Charts';
 import { useFetchCryptoAssetsInCurrency } from '../../hooks/useFetchCryptoAssetsInCurrency';
 import { Loader } from '../../components/Loaders/Loader';
+import { useSelector } from 'react-redux';
+import { selectCryptoCoins } from '../../store/selectors';
 
 export const CryptoLayout = () => {
 	const { cryptoAssetsInCurrency, isLoading } = useFetchCryptoAssetsInCurrency();
+	const cryptoCoins = useSelector(selectCryptoCoins);
 
 	return isLoading ? (
 		<Loader />
 	) : (
 		<section className="flex flex-col flex-20/24 gap-4">
-			<TopRowCardsLayout cryptoAssetsInCurrency={cryptoAssetsInCurrency} isLoading={isLoading} />
+			<TopRowCardsLayout cryptoCoins={cryptoCoins} />
 			<div id="layout__crypto" className="flex flex-col flex-6/12 rounded-4xl gap-4">
 				<div id="crypto__top-container" className="flex flex-5/12 flex-row gap-4">
 					<CryptoResultLayout
