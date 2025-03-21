e494bb03278bac9ca70c88483a9935d6540d454e
Jkerdley <jk-erley@yandex.ru>
2025-03-21 23:27:20 +0700

    Добавил модальные окна в крипто активы + модалка для изменения актива

---
+++ frontend/src/components/modalWindow/AddCryptoAssetModal.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BaseModal } from './base/BaseModal';
+import { request } from '../../utils';
+import { fetchAccounts } from '../../store/actions/async';
+import { AccountForm } from './forms/AccountForm';
+
+export const AddCryptoAssetModal = ({ isOpen, onClose }) => {
+	const [formData, setFormData] = useState({
+		name: '',
+		balance: '',
+		icon: 'debit',
+		type: 'debit',
+	});
+	const [error, setError] = useState('');
+	const dispatch = useDispatch();
+
+	const handleTypeChange = (type, icon) => {
+		setFormData((prev) => ({
+			...prev,
+			type,
+			icon,
+		}));
+	};
+
+	const handleInputChange = (e) => {
+		const value = e.target.value;
+
+		if (value === '' || !isNaN(value)) {
+			setFormData((prev) => ({
+				...prev,
+				balance: value,
+			}));
+		} else {
+			alert('Пожалуйста, введите цифры');
+		}
+	};
+
+	const handleNameChange = (event) => {
+		const value = event.target.value;
+		setFormData((prev) => ({
+			...prev,
+			name: value,
+		}));
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const nameValue = formData.name;
+		const balanceValue = Number(formData.balance);
+
+		if (nameValue.length === 0) {
+			alert('Название счета не может быть пустым');
+		}
+
+		if (!isNaN(balanceValue) && balanceValue >= 0) {
+			try {
+				await request('/accounts', 'POST', {
+					...formData,
+					balance: balanceValue,
+				});
+				dispatch(fetchAccounts());
+				onClose();
+			} catch (err) {
+				setError(err.message);
+			}
+		} else {
+			alert('Баланс должен быть числом и больше нуля');
+		}
+	};
+
+	return (
+		<BaseModal isOpen={isOpen} onClose={onClose} width="md:w-[60vw] w-[90vw]" position="center">
+			<section className="flex flex-col p-6 h-full w-full">
+				<AccountForm
+					formData={formData}
+					handleSubmit={handleSubmit}
+					handleInputChange={handleInputChange}
+					handleNameChange={handleNameChange}
+					handleTypeChange={handleTypeChange}
+					error={error}
+					onClose={onClose}
+				/>
+			</section>
+		</BaseModal>
+	);
+};
--- frontend/src/pages/CryptoPage/CryptoPortfolioList/MyCriptoPortfolioList.jsx
+++ frontend/src/pages/CryptoPage/CryptoPortfolioList/MyCriptoPortfolioList.jsx
@@ -5,13 +5,30 @@ import { OperationsPanel } from '../../../components/OperationsPanelButtons/Oper
 import { SectionContainerHeader } from '../../../components/SectionContainerHeader/SectionContainerHeader';
 import { EditAddDeleteButton } from '../../../components/buttons';
 import { Loader } from '../../../components/Loaders/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	closeAddCryptoAssetModal,
+	closeUpdateCryptoAssetModal,
+	openAddCryptoAssetModal,
+} from '../../../store/actions';
+import { selectAddCryptoAssetModal, selectUpdateCryptoAssetModal } from '../../../store/selectors';
+import { AddCryptoAssetModal, UpdateCryptoAssetModal } from '../../../components/modalWindow';
 
 export const MyCriptoPortfolioList = ({ cryptoAssetsInCurrency, isLoading }) => {
+	const addCryptoAsset = useSelector(selectAddCryptoAssetModal);
+	const updateCryptoAsset = useSelector(selectUpdateCryptoAssetModal);
+	const dispatch = useDispatch();
+
 	return (
 		<section id="column__categories" className="flex flex-col flex-3/12 p-4 rounded-3xl bg-sky-950/40">
 			<div id="categories__title-and-buitton" className="flex justify-between gap-2 mb-2">
 				<SectionContainerHeader title={'Активы'} />
-				<EditAddDeleteButton icon={AddIcon} title={'Добавить'} alt={'crypto coins'} />
+				<EditAddDeleteButton
+					onClick={() => dispatch(openAddCryptoAssetModal())}
+					icon={AddIcon}
+					title={'Добавить'}
+					alt={'crypto coins'}
+				/>
 			</div>
 			{isLoading ? (
 				<div className="flex flex-4/12 pr-2 justify-between 2xl:max-h-[36vh] min-w-[22vw] w-full h-full">
@@ -22,10 +39,26 @@ export const MyCriptoPortfolioList = ({ cryptoAssetsInCurrency, isLoading }) => 
 					id="spend-categories__container"
 					className="flex flex-4/12 flex-wrap gap-4 pr-2 justify-between 3xl:max-h-[36vh] max-h-[30vh] w-full h-full rounded-2xl overflow-y-auto overscroll-auto scroll-smooth scrollbar"
 				>
+					{addCryptoAsset.isOpen && (
+						<AddCryptoAssetModal
+							isOpen={addCryptoAsset.isOpen}
+							onClose={() => dispatch(closeAddCryptoAssetModal())}
+						/>
+					)}
+					{updateCryptoAsset.isOpen && (
+						<UpdateCryptoAssetModal
+							cryptoAssetsInCurrency={cryptoAssetsInCurrency}
+							assetId={updateCryptoAsset.cryptoAssetIdForUpdate}
+							isOpen={updateCryptoAsset.isOpen}
+							onClose={() => dispatch(closeUpdateCryptoAssetModal())}
+						/>
+					)}
+
 					{cryptoAssetsInCurrency.map((coin) => {
 						return (
 							<CryptoAssets
 								key={coin.id}
+								id={coin.id}
 								averageBuyPrice={coin.averagePrice}
 								assetsAmount={coin.assetAmount}
 								coinPrice={coin.price}
