6d9c2c563c2581ad4c5c8e316b17aa9914642bc2
Jkerdley <jk-erley@yandex.ru>
2025-03-26 22:03:42 +0700

    feat(crypto): Добавил кнопку  удаления истории крипто-актива
    Также реализовал модальное окно и удаление на бекенде

--- backend/controllers/cryptoassets.js
+++ backend/controllers/cryptoassets.js
@@ -1,9 +1,120 @@
 const { default: mongoose } = require("mongoose");
 const CryptoAssets = require("../models/CryptoAssets");
+
 async function getCryptoAssets(userId) {
     const cryptoAssets = await CryptoAssets.find({ userId });
     return cryptoAssets;
 }
+
+async function deleteCryptoAssetHistoryItem(userId, assetId, historyItemId) {
+    console.log("userId, assetId, historyItemId", userId, assetId, historyItemId);
+    const session = await mongoose.startSession();
+
+    try {
+        const oldAsset = await CryptoAssets.findOneAndUpdate(
+            {
+                userId: userId,
+                coinId: assetId,
+            },
+            {
+                $pull: {
+                    history: { _id: historyItemId },
+                },
+            }
+        );
+        if (!oldAsset) {
+            throw new Error("Ошибка: Актив не найден");
+        }
+        const deletedHistoryItemId = oldAsset.history.find((item) => item._id === historyItemId);
+        console.log("deletedHistoryItemId", deletedHistoryItemId);
+        session.startTransaction();
+        const result = await CryptoAssets.findOneAndUpdate(
+            {
+                _id: oldAsset._id,
+                userId,
+            },
+            {
+                $inc: { assetAmount: -deletedHistoryItemId.assetAmount },
+            }
+        );
+        console.log("result", result);
+        return result;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+async function addHistoryItem(data, userId) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    if (data.type === "spend") {
+        try {
+            const historyItem = await History.create([{ ...data, userId }], { session });
+
+            const account = await Accounts.findByIdAndUpdate(
+                {
+                    _id: data.accountId,
+                    userId,
+                },
+                { $inc: { balance: -data.amount } },
+                { new: true, session }
+            );
+            console.log("Обновленный счет:", account);
+
+            if (!account) throw new Error("Счет не найден");
+            if (account.balance < 0) throw new Error("Недостаточно средств на балансе");
+
+            const category = await Categories.findByIdAndUpdate(
+                {
+                    _id: data.categoryId,
+                    userId,
+                },
+                {
+                    $inc: { balance: data.amount },
+                },
+                { new: true, session }
+            );
+
+            if (!category) throw new Error("Категория расходов не найдена в базе данных");
+
+            await session.commitTransaction();
+            return historyItem[0];
+        } catch (error) {
+            await session.abortTransaction();
+            throw error;
+        } finally {
+            session.endSession();
+        }
+    } else if (data.type === "add") {
+        try {
+            const historyItem = await History.create([{ ...data, userId }], { session });
+
+            const account = await Accounts.findByIdAndUpdate(
+                {
+                    _id: data.accountId,
+                    userId,
+                },
+                { $inc: { balance: data.amount } },
+                { new: true, session }
+            );
+            console.log("Обновленный счет:", account);
+
+            if (!account) throw new Error("Счет не найден");
+
+            await session.commitTransaction();
+            return historyItem[0];
+        } catch (error) {
+            await session.abortTransaction();
+            throw error;
+        } finally {
+            session.endSession();
+        }
+    }
+}
+
 module.exports = {
     getCryptoAssets,
+    deleteCryptoAssetHistoryItem,
 };
--- frontend/src/components/modalWindow/UpdateCryptoAssetModal.jsx
+++ frontend/src/components/modalWindow/UpdateCryptoAssetModal.jsx
@@ -2,52 +2,40 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { BaseModal } from './base/BaseModal';
 import { request } from '../../utils';
-import { CategoriesForm } from './forms';
+import { CryptoAssetUpdate } from './forms';
 import DeleteIcon from '../../assets/icons/delete-icon.svg';
 import OutlineButton from '../buttons/OutlineButton';
 
 export const UpdateCryptoAssetModal = ({ isOpen, onClose, assetId, cryptoAssetsInCurrency }) => {
-	const selectedAsset = cryptoAssetsInCurrency.find((asset) => asset.id === assetId);
-	// const transformedCategory = {
-	// 	...selectedAsset,
-	// 	budget: parseFloat(selectedAsset.budget.slice(1).trim()),
-	// };
-	console.log('selectedAsset', selectedAsset);
-
-	const [formData, setFormData] = useState({
-		name: selectedAsset.name,
-		assetAmount: selectedAsset.assetAmount,
-		icon: selectedAsset.icon,
-	});
-
 	const [error, setError] = useState('');
 	const dispatch = useDispatch();
+	const selectedAsset = cryptoAssetsInCurrency.find((asset) => asset.id === assetId);
 
-	const handleInputChange = (event) => {
-		const value = event.target.value;
+	console.log('selectedAsset', selectedAsset);
 
-		if (value === '' || !isNaN(value)) {
-			setFormData((prev) => ({
-				...prev,
-				assetAmount: value,
-			}));
-		} else {
-			alert('Пожалуйста, введите цифры');
-		}
-	};
+	// const [formData, setFormData] = useState({
+	// 	name: selectedAsset.name,
+	// 	assetAmount: selectedAsset.assetAmount,
+	// 	icon: selectedAsset.icon,
+	// });
 
-	const handleNameChange = (event) => {
-		const value = event.target.value;
-		setFormData((prev) => ({
-			...prev,
-			name: value,
-		}));
-	};
+	// const handleInputChange = (event) => {
+	// 	const value = event.target.value;
+
+	// 	if (value === '' || !isNaN(value)) {
+	// 		setFormData((prev) => ({
+	// 			...prev,
+	// 			assetAmount: value,
+	// 		}));
+	// 	} else {
+	// 		alert('Пожалуйста, введите цифры');
+	// 	}
+	// };
 
 	const handleDeleteCategory = async () => {
-		if (confirm('Вы уверены что хотите удалить категорию расходов?')) {
+		if (confirm('Вы уверены что хотите удалить криптоактив?')) {
 			try {
-				await request(`/categories/${assetId}`, 'DELETE');
+				await request(`/cryptoassets/${assetId}`, 'DELETE');
 				// dispatch(fetchCategories());
 				onClose();
 			} catch (error) {
@@ -56,44 +44,36 @@ export const UpdateCryptoAssetModal = ({ isOpen, onClose, assetId, cryptoAssetsI
 		}
 	};
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
+	// const handleSubmit = async (event) => {
+	// 	event.preventDefault();
 
-		const nameValue = formData.name;
+	// 	const nameValue = formData.name;
 
-		if (nameValue.length === 0) {
-			alert('Название категории не может быть пустым');
-		}
-		const assetAmountValue = Number(formData.assetAmount);
+	// 	if (nameValue.length === 0) {
+	// 		alert('Название категории не может быть пустым');
+	// 	}
+	// 	const assetAmountValue = Number(formData.assetAmount);
 
-		if (!isNaN(assetAmountValue) && assetAmountValue >= 0) {
-			try {
-				await request(`/crypto/${assetId}`, 'PUT', {
-					...formData,
-					assetAmount: assetAmountValue,
-				});
-				// dispatch(fetchCategories());
-				onClose();
-			} catch (error) {
-				setError(error.message);
-			}
-		} else {
-			alert('Бюджет должен быть числом и больше нуля');
-		}
-	};
+	// 	if (!isNaN(assetAmountValue) && assetAmountValue >= 0) {
+	// 		try {
+	// 			await request(`/crypto/${assetId}`, 'PUT', {
+	// 				...formData,
+	// 				assetAmount: assetAmountValue,
+	// 			});
+	// 			// dispatch(fetchCategories());
+	// 			onClose();
+	// 		} catch (error) {
+	// 			setError(error.message);
+	// 		}
+	// 	} else {
+	// 		alert('Бюджет должен быть числом и больше нуля');
+	// 	}
+	// };
 
 	return (
 		<BaseModal isOpen={isOpen} onClose={onClose} width="md:w-[60vw] w-[90vw]" position="center">
 			<section className="flex flex-col justify-center p-4 h-full w-full">
-				<CategoriesForm
-					formData={formData}
-					handleSubmit={handleSubmit}
-					handleInputChange={handleInputChange}
-					handleNameChange={handleNameChange}
-					error={error}
-					onClose={onClose}
-					isUpdateForm={true}
-				/>
+				<CryptoAssetUpdate selectedAsset={selectedAsset} error={error} onClose={onClose} />
 
 				<div className="flex justify-center mb-4">
 					<OutlineButton icon={DeleteIcon} onClick={handleDeleteCategory}>
